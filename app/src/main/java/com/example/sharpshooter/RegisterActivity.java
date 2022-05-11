5848fd939545810bac57545f49c94bb8e894beca
Reisacher <oliver.reisacher@hs-weingarten.de>
2022-05-11 13:47:07 +0200

    Implemented login / register

---
+++ app/src/main/java/com/example/sharpshooter/RegisterActivity.java
@@ -0,0 +1,129 @@
+package com.example.sharpshooter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.sql.SQLOutput;
+
+public class RegisterActivity extends AppCompatActivity
+{
+    private FirebaseAuth mAuth;
+    private EditText name_input;
+    private EditText email_input;
+    private EditText password_input;
+    private EditText password_repeat_input;
+    private TextView error;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_register);
+
+        // Authentication setup
+        mAuth = FirebaseAuth.getInstance();
+
+        name_input = (EditText) findViewById(R.id.name);
+        email_input = (EditText) findViewById(R.id.email);
+        password_input = (EditText) findViewById(R.id.password);
+        password_repeat_input = (EditText) findViewById(R.id.password_repeat);
+        error = (TextView) findViewById(R.id.tv_error);
+
+        error.setVisibility(View.INVISIBLE);
+
+        Button btn_register = (Button) findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Get data from input fields
+                String name = name_input.getText().toString();
+                String email = email_input.getText().toString();
+                String password = password_input.getText().toString();
+                String password_repeat = password_repeat_input.getText().toString();
+
+                // Check if all input fields are filled
+                if (name.isEmpty() || email.isEmpty() || password.isEmpty() || password_repeat.isEmpty())
+                    return;
+
+                // Check if email is valid
+                if (!email.contains("@"))
+                {
+                    showError(R.string.login_failed_auth_email_invalid);
+                    return;
+                }
+
+                // Check if password is at least 8 characters long
+                if (password.length() < 8)
+                {
+                    showError(R.string.register_failed_auth_password_length);
+                    return;
+                }
+
+                // Check if password and password repeat matches
+                if (!password.equals(password_repeat))
+                {
+                    showError(R.string.register_failed_auth_password_missmatch);
+                    return;
+                }
+
+                registerUser(name, email, password);
+            }
+        });
+    }
+
+    private void registerUser(String name, String email, String password)
+    {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    // Get User
+                    FirebaseUser user = mAuth.getCurrentUser();
+
+                    // Start main activity
+                    Intent main_intent = new Intent(RegisterActivity.this, MainActivity.class);
+
+                    // TODO -> Initialise Firebase Structure
+
+                    startActivity(main_intent);
+
+                    // Close all previous intents
+                    finishAffinity();
+                } else {
+                    // Reset fields and show error
+                    email_input.setText("");
+
+                    if (task.getException().getMessage().contains("email"))
+                    {
+                        showError(R.string.register_failed_auth_email_used);
+                    }
+                    else
+                    {
+                        showError(R.string.register_failed_auth_unknown_error);
+                        name_input.setText("");
+                        password_input.setText("");
+                        password_repeat_input.setText("");
+                    }
+                }
+            }
+        });
+    }
+
+    private void showError(int path)
+    {
+        error.setText(path);
+        error.setVisibility(View.VISIBLE);
+    }
+}
--- app/src/main/java/com/example/sharpshooter/ui/account/AccountFragment.java
+++ app/src/main/java/com/example/sharpshooter/ui/account/AccountFragment.java
@@ -1,32 +1,51 @@
 package com.example.sharpshooter.ui.account;
 
+import static androidx.core.app.ActivityCompat.finishAffinity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.sharpshooter.MainActivity;
+import com.example.sharpshooter.WelcomeActivity;
 import com.example.sharpshooter.databinding.FragmentAccountBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class AccountFragment extends Fragment {
 
     private FragmentAccountBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        AccountViewModel accountViewModel =
-                new ViewModelProvider(this).get(AccountViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        AccountViewModel accountViewModel = new ViewModelProvider(this).get(AccountViewModel.class);
 
         binding = FragmentAccountBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textAccount;
-        accountViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        Button btn_logout = binding.button;
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Sign out current user
+                FirebaseAuth mAuth = FirebaseAuth.getInstance();
+                mAuth.signOut();
+
+                // "Restart" application by loading welcome page
+                Intent welcome_intent = new Intent(getActivity(), WelcomeActivity.class);
+                startActivity(welcome_intent);
+                finishAffinity(getActivity());
+            }
+        });
+
         return root;
     }
 
