d4e4271fd5b9e3c32cedd03d1d0cf3723b1fdd4d
Oli <reisacher22@gmail.com>
2022-06-01 09:54:54 +0200

    Active game is not separated

--- app/src/main/java/com/example/sharpshooter/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/sharpshooter/ui/home/HomeFragment.java
@@ -13,6 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 
+import com.example.sharpshooter.FirebaseUtil;
 import com.example.sharpshooter.ui.NewGameDialog;
 import com.example.sharpshooter.ui.NewParkourDialog;
 import com.example.sharpshooter.ui.PlayerInputDialog;
@@ -31,8 +32,10 @@ import java.util.ArrayList;
 public class HomeFragment extends Fragment {
 
     private RecyclerView lastGameRV;
+    private RecyclerView activeGameRV;
 
     private ArrayList<LastGameModel> lastGameModelArrayList;
+    private ArrayList<LastGameModel> activeGameModelArrayList;
 
     private FragmentHomeBinding binding;
 
@@ -45,30 +48,39 @@ public class HomeFragment extends Fragment {
 
         // Card with Recycler view
         lastGameRV = root.findViewById(R.id.lastGameRecyclerView);
+        activeGameRV = root.findViewById(R.id.activeGameRecyclerView);
 
         // dummy data.
         lastGameModelArrayList = new ArrayList<>();
+        activeGameModelArrayList = new ArrayList<>();
+
         FirebaseUtil.getInstance().database.collection("users").document(FirebaseUtil.getInstance().authentication.getUid()).collection("games").addSnapshotListener((value, error) -> {
-            System.out.println(value.getDocuments().get(0).get("gameName"));
-            System.out.println(value.getDocuments().get(0).get("targetCount"));
-            System.out.println(value.getDocuments().size());
-            for (int i = 0; i < value.getDocuments().size(); i++) {
-                if (value.getDocuments().get(i).get("active").toString() == "true")
-                    lastGameModelArrayList.add(0,new LastGameModel("ActiveGame", "01.01.2022", 3, Integer.parseInt(value.getDocuments().get(i).get("targetCount").toString()), R.drawable.ic_account_black_24dp));
-                else
-                    lastGameModelArrayList.add(new LastGameModel(value.getDocuments().get(i).get("gameName").toString(), "01.01.2022", 3, Integer.parseInt(value.getDocuments().get(i).get("targetCount").toString()), R.drawable.ic_account_black_24dp));
+
+            if (value.getDocuments().size() > 0)
+            {
+                for (int i = 0; i < value.getDocuments().size(); i++) {
+                    if (value.getDocuments().get(i).get("active").toString() == "true")
+                        activeGameModelArrayList.add(0,new LastGameModel("ActiveGame", "01.01.2022", 3, Integer.parseInt(value.getDocuments().get(i).get("targetCount").toString()), R.drawable.ic_account_black_24dp));
+                    else
+                        lastGameModelArrayList.add(new LastGameModel(value.getDocuments().get(i).get("gameName").toString(), "01.01.2022", 3, Integer.parseInt(value.getDocuments().get(i).get("targetCount").toString()), R.drawable.ic_account_black_24dp));
+                }
             }
+
             // we are initializing our adapter class and passing our arraylist to it.
             LastGameAdapter lastGameAdapter = new LastGameAdapter(root.getContext(), lastGameModelArrayList);
+            LastGameAdapter activeGameAdapter = new LastGameAdapter(root.getContext(), activeGameModelArrayList);
 
             // below line is for setting a layout manager for our recycler view.
             // here we are creating vertical list so we will provide orientation as vertical
             LinearLayoutManager linearLayoutManager = new LinearLayoutManager(root.getContext(), LinearLayoutManager.VERTICAL, false);
+            LinearLayoutManager linearLayoutManager2 = new LinearLayoutManager(root.getContext(), LinearLayoutManager.VERTICAL, false);
 
             // in below two lines we are setting layoutmanager and adapter to our recycler view.
             lastGameRV.setLayoutManager(linearLayoutManager);
             lastGameRV.setAdapter(lastGameAdapter);
 
+            activeGameRV.setLayoutManager(linearLayoutManager2);
+            activeGameRV.setAdapter(activeGameAdapter);
         });
 
 
@@ -84,43 +96,6 @@ public class HomeFragment extends Fragment {
         NewGameDialog newGameDialog = new NewGameDialog(R.layout.dialog_newgame, newParkourDialog);
 
         newGameDialog.show(getParentFragmentManager(), "newGameDialog");
-
-        /*
-        Dialog dialog = new Dialog(getActivity());
-        dialog.setContentView(R.layout.dialog_newgame);
-        dialog.getWindow().setBackgroundDrawableResource(R.drawable.dialog_background);
-        Button startBtn = dialog.findViewById(R.id.newGame_newParkour);
-        startBtn.setOnClickListener(view -> {
-            Dialog newParkourDialog = new Dialog(getActivity());
-            newParkourDialog.setContentView(R.layout.dialog_newparkour);
-            newParkourDialog.getWindow().setBackgroundDrawableResource(R.drawable.dialog_background);
-
-            Button continueBtn = newParkourDialog.findViewById(R.id.newParkour_continue);
-            continueBtn.setOnClickListener(view2 -> {
-                Dialog newParkourPlayerNames = new Dialog(getActivity());
-                newParkourPlayerNames.setContentView(R.layout.dialog_newparkour_playernames);
-                newParkourPlayerNames.getWindow().setBackgroundDrawableResource(R.drawable.dialog_background);
-
-                newParkourPlayerNames.show();
-
-                playerNameDialogRV = view2.findViewById(R.id.recyclerViewPlayerNames);
-                PlayerNameDialogAdapter playerNameDialogAdapter = new PlayerNameDialogAdapter(view2.getContext());
-                System.out.println(playerNameDialogAdapter.getItemCount());
-                playerNameDialogRV.setAdapter(playerNameDialogAdapter);
-
-
-
-                newParkourDialog.dismiss();
-            });
-
-            newParkourDialog.show();
-            // MainActivity.setBottomNavVisibility(true, (MainActivity) getActivity());
-            // MainActivity.replaceFragment( (MainActivity) getActivity());
-            dialog.dismiss();
-        });
-
-        dialog.show();
-        */
     }
 
     @Override
