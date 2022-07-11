7bf99fe060598ea28495827f25e2c7a5fd27924a
Reisacher <oliver.reisacher@hs-weingarten.de>
2022-07-11 16:07:31 +0200

    Added back buttons to login / register view. Added new progress bar design.

--- app/src/main/java/com/example/sharpshooter/RegisterActivity.java
+++ app/src/main/java/com/example/sharpshooter/RegisterActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -84,6 +85,14 @@ public class RegisterActivity extends AppCompatActivity
                 }
 
                 registerUser(name, email, password);
+            }
+        });
+
+        ImageButton btn_back = (ImageButton) findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
